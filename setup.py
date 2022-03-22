341b60f74cd103997cf1795e8be2345b23720a9d
Harold Hannon <haroldhannon@gmail.com>
2022-03-22 10:38:14 -0500

    Add files via upload
    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='vault_client',
+    version='0.1',
+    packages=['vault_client'],
+    url='',
+    license='none',
+    author='haroldhannon',
+    author_email='hhannon@us.ibm.com',
+    description='quick vault client to access secrets'
+)
